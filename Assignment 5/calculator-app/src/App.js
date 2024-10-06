5f4d22686699f680b1e7b612e3a93e36c1b469f0
Vicky Singh Rajput <140153819+VickyRajput23@users.noreply.github.com>
2024-10-06 23:43:15 +0530

    Add files via upload

---
+++ Assignment 5/calculator-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Calculator from './components/Calculator';
+
+function App() {
+  return (
+    <div>
+      <div className="container my-3">
+      <Calculator />
+      </div>
+    </div>
+  );
+}
+
+export default App;
